570455b32b1cbacabd5d7399e24eb385135e1b26
GHada Mohsen <mohsenghada29@gmail.com>
2026-09-07 00:56:00 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -30,15 +30,6 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyWidget(),
     );
-  }
-}
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
