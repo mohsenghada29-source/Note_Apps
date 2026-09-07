275a0fe68b555a679094098a80f06ceaac64418f
GHada Mohsen <mohsenghada29@gmail.com>
2026-09-07 03:11:50 +0300

    add floating action button

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -8,6 +8,7 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 body: NotsViewBody(),
+floatingActionButton: FloatingActionButton(onPressed: (){},child: const Icon(Icons.add),),
     );
   }
 }
--- lib/views/widgets/note_view_body.dart
+++ lib/views/widgets/note_view_body.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/custom_appbar.dart';
 import 'package:note_app/views/widgets/custom_note_item.dart';
+import 'package:note_app/views/widgets/notes_list_view.dart';
 
 class NotsViewBody extends StatelessWidget {
   const NotsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding
-    (
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(children: [
-        SizedBox(
-          height: 50,
-        ),
-      CustomAppbar(),
-      NoteItem(),
-      ]),
+      child: Column(children: [SizedBox(height: 50), CustomAppbar(),
+        Expanded(child: NotesListView()),]),
+    
     );
   }
 }
+
